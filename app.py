3da4bf8b8a3b1c3f4a8384f4c8aec4fade8741cf
Mike Collins <mike.d.collins@gmail.com>
2016-02-25 17:04:43 -0800

    updates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 
 client = ZillowClient("X1-ZWz19v0zahxc7f_4eq90")
 
-now = datetime.date.today().strftime("%d/%m/%Y,")
+now = datetime.date.today().strftime("%m/%d/%Y,")
 price = 0.0
 count = 0
 
@@ -21,32 +21,42 @@ with open("addresses.txt") as addresses:
                 else :
                     a = a.split('|')
                     xml_response = client.get_search_results(a[0].strip(), a[1].strip())
-                    b = re.sub(r'.*<zestimate>', "", xml_response)
-                    b = re.sub(r'</zestimate>.*', "", b)
+                    err = re.match('.*Error: no exact match', xml_response)
 
-                    b = re.sub(r' currency="USD"', "", b)
-                    b = re.sub(r'<last-updated>.*</last-updated>', "", b)
-                    b = re.sub(r'<oneWeekChange.*</oneWeekChange>', "", b)
-                    b = re.sub(r'<valueChange.*</valueChange>', "", b)
-                    b = re.sub(r'<percentile.*</percentile>', "", b)
+                    if err :
+                        print 'Error: bad address', a[0], a[1]
+                    else :
+                        b = re.sub(r'.*<zestimate>', "", xml_response)
+                        b = re.sub(r'</zestimate>.*', "", b)
 
-                    b = re.sub(r'<valuationRange>', "", b)
-                    b = re.sub(r'</valuationRange>', "", b)
+                        b = re.sub(r' currency="USD"', "", b)
+                        b = re.sub(r'<last-updated>.*</last-updated>', "", b)
+                        b = re.sub(r'<oneWeekChange.*</oneWeekChange>', "", b)
+                        b = re.sub(r'<valueChange.*</valueChange>', "", b)
+                        b = re.sub(r'<percentile.*</percentile>', "", b)
 
-                    b = re.sub(r'<amount>', "", b)
-                    b = re.sub(r'</amount>', ",", b)
-                    b = re.sub(r'<low>', "", b)
-                    b = re.sub(r'</low>', ",", b)
+                        b = re.sub(r'<valuationRange>', "", b)
+                        b = re.sub(r'</valuationRange>', "", b)
 
-                    b = re.sub(r'<high>', "", b)
-                    b = re.sub(r'</high>', ",", b)
+                        b = re.sub(r'<amount>', "", b)
+                        b = re.sub(r'</amount>', ",", b)
+                        b = re.sub(r'<low>', "", b)
+                        b = re.sub(r'</low>', ",", b)
 
-                    # should now have just csv
-                    (p, l, h) = [t(s) for t, s in zip((float, float, float), b.split(','))]
-                    price = price + p
-                    count = count + 1
+                        b = re.sub(r'<high>', "", b)
+                        b = re.sub(r'</high>', "", b)
 
-                    print now, b, a[0].strip(), ', ', a[1].strip()
+                        # b should now have just csv of zestimate prices
+
+                        # this from SO using comprehensions, don't quite follow
+                        # (p, l, h) = [t(s) for t, s in zip((float, float, float), b.split(','))]
+
+                        (p, l, h) = b.split(',')
+                        price += float(p)
+
+                        count += 1
+
+                        print now, b, ', ', a[0].strip(), ', ', a[1].strip()
 
 
 addresses.close()
